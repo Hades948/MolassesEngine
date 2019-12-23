4f968f8ede3c9958229f7bc6682dcd855a8e3c26
hades948 <hades948@gmail.com>
2019-12-22 23:57:10 -0500

    Expanded button initialization options

--- MolassesEngine/src/main/java/com/tylerroyer/enginetesting/TestScreen.java
+++ MolassesEngine/src/main/java/com/tylerroyer/enginetesting/TestScreen.java
@@ -3,21 +3,24 @@ package com.tylerroyer.enginetesting;
 import com.tylerroyer.molasses.*;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 class TestScreen extends Screen {
+    Button testButton;
     public TestScreen() {}
 
     @Override
-    public void loadResources() {}
+    public void loadResources() {
+        Font font = new Font("Helvetica", Font.PLAIN, 32);
+        testButton = new Button("Test", font, Color.RED, Color.GREEN, Color.BLUE, Color.BLACK, 100, 60, 200, 175);
+    }
 
     @Override
     public void update() {}
 
     @Override
     public void render(Graphics2D g) {
-        Game.setBackgroundColor(Color.BLACK);
-        g.setColor(Color.WHITE);
-        g.drawString("Test", 200, 200);
+        testButton.render(g);
     }
 }
--- MolassesEngine/src/main/java/com/tylerroyer/molasses/Button.java
+++ MolassesEngine/src/main/java/com/tylerroyer/molasses/Button.java
@@ -1,6 +1,8 @@
 package com.tylerroyer.molasses;
 
 import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 
@@ -14,6 +16,42 @@ public class Button {
         this.highlighted = highlighted;
         this.x = x;
         this.y = y;
+    }
+
+    public Button(String text, Font font, Color pressedColor, Color unpressedColor, Color highlightedColor, Color textColor,
+            int width, int height, int x, int y) {        
+        pressed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        unpressed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        highlighted = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D gPressed = pressed.createGraphics();
+        gPressed.setColor(pressedColor);
+        gPressed.fillRect(0, 0, width, height);
+        gPressed.setFont(font);
+        gPressed.setColor(textColor);
+        // TODO Try to center the text on the button.  (POE Code?)
+        gPressed.drawString(text, 10, height - 10);
+
+        Graphics2D gUnpressed = unpressed.createGraphics();
+        gUnpressed.setColor(unpressedColor);
+        gUnpressed.fillRect(0, 0, width, height);
+        gUnpressed.setFont(font);
+        gUnpressed.setColor(textColor);
+        gUnpressed.drawString(text, 10, height - 10);
+
+        Graphics2D gHighlighted = highlighted.createGraphics();
+        gHighlighted.setColor(highlightedColor.brighter());
+        gHighlighted.fillRect(0, 0, width, height);
+        gHighlighted.setFont(font);
+        gHighlighted.setColor(textColor);
+        gHighlighted.drawString(text, 10, height - 10);
+        
+        this.x = x;
+        this.y = y;
+    }
+
+    public Button(String text, Font font, Color backgroundColor, Color textColor, int width, int height, int x, int y) {        
+        this(text, font, backgroundColor.darker(), backgroundColor, backgroundColor.brighter(), textColor, width, height, x, y);
     }
 
     private boolean isMouseHovering() {
