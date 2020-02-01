0015de2bb41725332c324128ff49bf6aa5974b16
hades948 <hades948@gmail.com>
2020-01-31 23:09:04 -0500

    Added global font to config

--- MolassesEngine/src/main/java/com/tylerroyer/molasses/Config.java
+++ MolassesEngine/src/main/java/com/tylerroyer/molasses/Config.java
@@ -1,5 +1,7 @@
 package com.tylerroyer.molasses;
 
+import java.awt.Font;
+
 public class Config {
     public static double devScreenWidth = 1920.0;
     public static double devScreenHeight = 1080.0;
@@ -7,4 +9,5 @@ public class Config {
     public static int windowHeight = 500;
     public static String windowTitle = "Built on The Molasses Game Engine";
     public static Screen firstScreen = null;
+    public static Font gameFont = new Font("Helvetica", Font.PLAIN, 42);
 }
