a07be5e24ffa7a70d55be8b51024370c78462e7a
hades948 <hades948@gmail.com>
2020-01-31 22:59:53 -0500

    GameGraphics now handles cam offset rendering

--- MolassesEngine/src/main/java/com/tylerroyer/molasses/GameGraphics.java
+++ MolassesEngine/src/main/java/com/tylerroyer/molasses/GameGraphics.java
@@ -10,17 +10,22 @@ import java.awt.Stroke;
 
 public class GameGraphics {
     private Graphics2D g;
+    private Camera camera = null;
 
     public GameGraphics(Graphics2D g) {
         this.g = g;
     }
 
     public boolean drawImage(Image image, double x, double y, ImageObserver o) {
-        return g.drawImage(image, (int) (x * Resources.scaleX), (int) (y * Resources.scaleY), o);
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        return g.drawImage(image, (int) (x * Resources.scaleX + camX), (int) (y * Resources.scaleY + camY), o);
     }
 
     public boolean drawImage(Image image, double x, double y) {
-        return drawImage(image, x, y, Game.getWindow());
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        return drawImage(image, x + camX, y + camY, Game.getWindow());
     }
 
     public void setFont(Font font) {
@@ -36,6 +41,7 @@ public class GameGraphics {
     }
 
     public void setClip(Rectangle r) {
+        // TODO Should this be relative to the camera?
         Rectangle scaledRect = new Rectangle((int) (r.getX() * Resources.scaleX), (int) (r.getY() * Resources.scaleY),
                 (int) (r.getWidth() * Resources.scaleX), (int) (r.getHeight() * Resources.scaleY));
         g.setClip(scaledRect);
@@ -50,18 +56,30 @@ public class GameGraphics {
     }
 
     public void drawString(String text, double x, double y) {
-        g.drawString(text, (int) (x * Resources.scaleX), (int) (y * Resources.scaleY));
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        g.drawString(text, (int) (x * Resources.scaleX + camX), (int) (y * Resources.scaleY + camY));
     }
 
     public void fillRect(double x, double y, double width, double height) {
-        g.fillRect((int) (x * Resources.scaleX), (int) (y * Resources.scaleY), (int) (width * Resources.scaleX), (int) (height * Resources.scaleY));
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        g.fillRect((int) (x * Resources.scaleX + camX), (int) (y * Resources.scaleY + camY), (int) (width * Resources.scaleX), (int) (height * Resources.scaleY));
     }
 
     public void drawRect(double x, double y, double width, double height) {
-        g.drawRect((int) (x * Resources.scaleX), (int) (y * Resources.scaleY), (int) (width * Resources.scaleX),
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        g.drawRect((int) (x * Resources.scaleX + camX), (int) (y * Resources.scaleY + camY), (int) (width * Resources.scaleX),
                 (int) (height * Resources.scaleY));
     }
     public void drawLine(double x1, double y1, double x2, double y2) {
-        g.drawLine((int) (x1 * Resources.scaleX), (int) (y1 * Resources.scaleY), (int) (x2 * Resources.scaleX), (int) (y2 * Resources.scaleY));
+        double camX = camera == null ? 0 : camera.getOffsetX();
+        double camY = camera == null ? 0 : camera.getOffsetY();
+        g.drawLine((int) (x1 * Resources.scaleX + camX), (int) (y1 * Resources.scaleY + camY), (int) (x2 * Resources.scaleX), (int) (y2 * Resources.scaleY));
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
     }
 }
