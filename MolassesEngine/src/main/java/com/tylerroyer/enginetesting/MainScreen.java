0392e067e8f6ea6f57a87e3751425cb10a856752
hades948 <hades948@gmail.com>
2020-02-06 19:00:29 -0500

    Cleanup

--- MolassesEngine/src/main/java/com/tylerroyer/enginetesting/MainScreen.java
+++ MolassesEngine/src/main/java/com/tylerroyer/enginetesting/MainScreen.java
@@ -10,9 +10,13 @@ import com.tylerroyer.molasses.GameGraphics;
 import com.tylerroyer.molasses.Screen;
 import com.tylerroyer.molasses.events.*;
 
+import org.apache.commons.lang3.mutable.MutableInt;
+
 public class MainScreen extends Screen {
     Screen eventTestScreen;
     Button eventTestButton;
+    MutableInt tileX = new MutableInt(0);
+    MutableInt tileY = new MutableInt(0);
 
     public MainScreen() {
         eventTestScreen = new EventTestScreen();
@@ -23,8 +27,6 @@ public class MainScreen extends Screen {
         Font font = new Font("Helvetica", Font.PLAIN, 12);
         Event changeScreenEvent = new ChangeScreenEvent(eventTestScreen);
         eventTestButton = new Button("Test events", font, new Color(128, 0, 0), Color.WHITE, 150, 50, 25, 10, changeScreenEvent);
-
-        Game.getKeyboardHandler().addEvent(KeyEvent.VK_W, new LoggerEvent("W key pressed"));
     }
 
     @Override
